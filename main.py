5200acbd0eb6228186ba2084b65d152388b79ad8
nitelitevtuber <123321982+nitelitevtuber@users.noreply.github.com>
2024-02-13 20:20:31 -0700

    WOOOO I DID IT

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from twitchAPI.type import AuthScope
 import customtkinter
 import tkinter
 
+
+
 # grabs deets for use later 
 async def login_logic():
     # authenticates silly goofy little api thingie with the user
@@ -23,9 +25,16 @@ authwindow.title("Claire's Twitch Interaction Engine")
 authwindow.geometry("1920x1028")
 authwindow._state_before_windows_set_titlebar_color = 'zoomed'
 
-button = customtkinter.CTkButton(authwindow, text="my button",)
-button.grid(row=0, column=0, padx=890, pady=550, sticky="ew", columnspan=2)
-label = customtkinter.CTkLabel(authwindow, text="Please authenticate with Twitch to continue.", fg_color="transparent", font=("Segoe UI Bold", 50))
-label.grid(row=0, column=0, padx=930, pady=(0, 200), sticky="ew", columnspan=1)
+#i know this code is spagetti, but it works, and thats all i care about 
+button = customtkinter.CTkButton(authwindow, text="Authenticate ", command=login_logic,)
+button.grid(row=1, column=0, padx=0, pady=(0, 600))
+label = customtkinter.CTkLabel(authwindow, text="Please authenticate with Twitch to continue.", fg_color="transparent", font=("Segoe UI Bold", 30))
+label.grid_columnconfigure(0, weight=1)
+label.grid(row=0, column=0, padx=0, pady=(20, 0))
+authwindow.grid_rowconfigure(0, weight=1)
+authwindow.grid_rowconfigure(1, weight=1)
+authwindow.grid_columnconfigure(0, weight=1)
+
+
 authwindow.mainloop()
 
