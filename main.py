b20c5e7e315268aa6675cbbb257dd8eb3987d91d
nitelitevtuber <123321982+nitelitevtuber@users.noreply.github.com>
2024-02-16 16:14:55 -0700

    small little changes i guess

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from twitchAPI.twitch import Twitch
 from twitchAPI.oauth import UserAuthenticator
 from twitchAPI.type import AuthScope
 from twitchAPI.oauth import UserAuthenticationStorageHelper
+from twitchAPI.helper import first
 import customtkinter
 import tkinter
 import asyncio
 
 
-
+MOMOCHANNEL = 'sushifuu'
 
 # grabs deets for use later 
 async def login_logic():
@@ -21,6 +22,7 @@ async def login_logic():
     # then adds in the user's specific details to allow actual use of Twitch's APIs.
     token, refresh_token = await auth.authenticate()
     await twitch.set_user_authentication(token, target_scope, refresh_token)
+    user = await first(twitch.get_users(logins=[MOMOCHANNEL]))
     helper = UserAuthenticationStorageHelper(twitch, target_scope)
 
 def login_deez():
